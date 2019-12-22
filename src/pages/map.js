d928a63ca61877ec2dc66c89e4428859f29d95cd
Faisal Ahmed <33577124+FaisalAhmedBD@users.noreply.github.com>
2019-12-23 01:47:19 +0600

    Added current location Marker on Map

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,21 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react'
 import '../styles/map.scss'
 export default function Map({ center }) {
-    const [mapCenter, setMapCenter] = useState({ lat: 23.7508961, lng: 90.3842619 })
+    const [currentLocation, setCurrentLocation] = useState({ latitude: 23.7813293, longitude: 90.40274978 })
     const mapRef = useRef(null)
     useEffect(() => {
         initGoogleMap()
     })
 
     const initGoogleMap = () => {
-        new window.google.maps.Map(mapRef.current, {
+        let myLatlng = new window.google.maps.LatLng(23.7813293, 90.40274978)
+        let map = new window.google.maps.Map(mapRef.current, {
             zoom: 16,
-            center: new window.google.maps.LatLng(mapCenter.lat, mapCenter.lng)
+            center: new window.google.maps.LatLng(currentLocation.latitude, currentLocation.longitude)
         });
+        let marker = new window.google.maps.Marker({
+            position: myLatlng,
+            title: "Current Location"
+        })
+        marker.setMap(map)
+
+    }
+    const getCurrentLocation = () => {
+        if (navigator && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                console.log(position.coords)
+                setCurrentLocation(position.coords)
+            })
+        }
     }
 
     return (
-        <div className="map-container" ref={mapRef} />
+        <div>
+            <div className="map-container" ref={mapRef} >
+            </div>
+
+
+            <div onClick={() => getCurrentLocation()}>current location</div>
+        </div>
+
     )
+
 
 }
